3a5e27babb54372883ecb240276d8c77eb49e090
Becky <bekibahtiar21@gmail.com>
2025-09-15 19:21:58 +0700

    Update and rename src/supabaseClient.js to storage.js

--- storage.js
+++ storage.js
@@ -1,8 +1,8 @@
 // Import dari CDN (kalau plain HTML) → pastikan sudah ada di index.html
 // <script src="https://unpkg.com/@supabase/supabase-js"></script>
 
-const supabaseUrl = "https://xxxx.supabase.co";   // ganti dari Settings → API
-const supabaseKey = "eyJhbGciOiJIUzI1NiIs...";   // ganti pakai anon public key
+const supabaseUrl = "https://uuzhhkjzjdwbjhbhhpvk.supabase.co";   // ganti dari Settings → API
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1emhoa2p6amR3YmpoYmhocHZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5MzY0NTksImV4cCI6MjA3MzUxMjQ1OX0.lYWtAd7-Tw7he6Q3AvIOShC9e76TDsq_WhClbd-GrM4";   // ganti pakai anon public key
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 // Fungsi upload file
@@ -40,3 +40,4 @@ async function listFiles() {
     supabase.storage.from('namabucket').getPublicUrl('uploads/' + file.name).data.publicUrl
   );
 }
+
